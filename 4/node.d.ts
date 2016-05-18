425575e563c25da936f0c43bb3aa09f624f9ccdc
Stijn Van Nieuwenhuyse <stijn@drafthorse.io>
2016-05-18 09:49:14 +0200

    Use union type for msg parameter

--- 4/node.d.ts
+++ 4/node.d.ts
@@ -1362,14 +1362,10 @@ declare module "dgram" {
     export function createSocket(type: string, callback?: (msg: Buffer, rinfo: RemoteInfo) => void): Socket;
 
     interface Socket extends events.EventEmitter {
-        send(buf: Buffer, offset: number, length: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(buf: Buffer, offset: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(buf: Buffer, length: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(buf: Buffer, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(str: string, offset: number, length: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(str: string, offset: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(str: string, length: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
-        send(str: string, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
+        send(msg: Buffer | string | Array<any>, offset: number, length: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
+        send(msg: Buffer | string | Array<any>, offset: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
+        send(msg: Buffer | string | Array<any>, length: number, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
+        send(msg: Buffer | string | Array<any>, port: number, address: string, callback?: (error: Error, bytes: number) => void): void;
         bind(port: number, address?: string, callback?: () => void): void;
         close(): void;
         address(): AddressInfo;

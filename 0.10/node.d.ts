ca0aafe890db5365d2fc3c18ba5bdbb2e73cf850
Louay Alakkad <louay@alakkad.me>
2016-08-19 10:08:03 +0100

    Add 'module' module

--- 0.10/node.d.ts
+++ 0.10/node.d.ts
@@ -144,6 +144,10 @@ declare namespace NodeJS {
         stderr: WritableStream;
         stdin: ReadableStream;
         argv: string[];
+        /**
+         * The process.execArgv property returns the set of Node.js-specific command-line options passed when the Node.js process was launched. These options do not appear in the array returned by the process.argv property, and do not include the Node.js executable, the name of the script, or any options following the script name. These options are useful in order to spawn child processes with the same execution environment as the parent.
+         */
+        execArgv: string[];
         execPath: string;
         abort(): void;
         chdir(directory: string): void;
@@ -1408,4 +1412,21 @@ declare module "domain" {
     }
 
     export function create(): Domain;
+}
+
+declare module 'module' {
+  class Module {
+    static runMain (): void
+    static wrap (code: string): string
+    static _nodeModulePaths (path: string): string[]
+
+    constructor (filename: string)
+
+    filename: string
+    paths: string[]
+    exports: any
+    require (module: string): any
+  }
+
+  export = Module
 }
